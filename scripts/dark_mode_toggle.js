4a45f97c730e9aa0455447ff77f20b990c3ec563
vacuitydev <122996753+vacuitydev@users.noreply.github.com>
2023-06-16 12:37:26 +0500

    Add files via upload

--- scripts/dark_mode_toggle.js
+++ scripts/dark_mode_toggle.js
@@ -1,7 +1,6 @@
 window.onload = ()=>{
     const dark_mode_toggle =document.getElementById("dark-mode-toggle")
     dark_mode_toggle.onclick =()=>{
-        console.log("toggle")
         document.body.classList.toggle('dark')
     }
 }
